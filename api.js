2f66a4ddee5aa042e492a58414390a7d895d5085
Enkiduzis <bljoaogabriel@gmail.com>
2024-05-14 10:02:26 -0300

    debugando o git

--- api.js
+++ api.js
@@ -2,12 +2,13 @@ const express = require('express');
 const crypto = require('crypto');
 const fs = require('fs');
 const path = require('path');
-const fetch = require('node-fetch'); // Adicionar esta linha para importar o módulo 'node-fetch'
 
 const app = express();
 
-// URL do bucket do Cloudflare R2 onde os arquivos estão armazenados
-const bucketUrl = 'https://seu-dominio.com';
+// Diretório onde os arquivos estão armazenados
+// Alterar para o diretório onde os arquivos estão armazenados efetivamente
+// Exemplo de diretório: 'C:/xampp/htdocs/api-test/file'
+const fileDirectory = 'C:/xampp/htdocs/api-test/file';
 
 // Chave de criptografia (deve ter 32 caracteres para AES-256)
 // Alterar para uma chave segura e não compartilhar
@@ -23,22 +24,25 @@ function generateAccessToken(expirationMinutes = 5) {
 }
 
 // Rota para obter o arquivo criptografado com token de acesso
-app.get('/file/:filename', async (req, res) => {
+// Exemplo de URL: http://localhost:3000/file/arquivo.txt
+// Irá retornar a URL para download do arquivo criptografado
+// Exemplo de URL de download: http://localhost:3000/encrypted/LWvz1KPU7p/6pX8gErA5Xw==?token=eyJleHAiOjE3MTU2NDU2NTF9&iv=g9gMal_ayBj4R5uXiWyKIQ&filename=your-file.txt
+app.get('/file/:filename', (req, res) => {
   const { filename } = req.params;
   const token = generateAccessToken();
-  const fileUrl = `${bucketUrl}/${filename}`;
+  const filePath = path.join(fileDirectory, filename);
 
   try {
-    const response = await fetch(fileUrl);
-    const fileData = await response.buffer();
+    const fileData = fs.readFileSync(filePath);
     const iv = crypto.randomBytes(16); // Vetor de inicialização aleatório
     const cipher = crypto.createCipheriv('aes-256-cbc', encryptionKey, iv);
     let encrypted = cipher.update(fileData);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     const encryptedFile = encrypted.toString('base64');
 
-    const encryptedFileUrl = `${req.protocol}://${req.get('host')}/encrypted/${encryptedFile}?token=${token}&iv=${iv.toString('base64url')}&filename=${encodeURIComponent(filename)}`;
-    res.send(encryptedFileUrl);
+    const fileExt = path.extname(filename);
+	//Altere o servidor para o seu servidor de hospedagem
+    res.send(`http://localhost:3000/encrypted/${encryptedFile}?token=${token}&iv=${iv.toString('base64url')}&filename=${encodeURIComponent(filename)}`);
   } catch (err) {
     console.error(err);
     res.status(500).send('Erro ao obter o arquivo');
@@ -46,7 +50,9 @@ app.get('/file/:filename', async (req, res) => {
 });
 
 // Rota para validar o token de acesso e descriptografar o arquivo
-app.get('/encrypted/:encryptedFile', async (req, res) => {
+// Irá retornar o arquivo para download com o nome original
+// Exemplo de URL: http://localhost:3000/encrypted/LWvz1KPU7p/6pX8gErA5Xw==?token=eyJleHAiOjE3MTU2NDU2NTF9&iv=g9gMal_ayBj4R5uXiWyKIQ&filename=your-file.txt
+app.get('/encrypted/:encryptedFile', (req, res) => {
   const { encryptedFile } = req.params;
   const { token, iv, filename } = req.query;
 
@@ -62,7 +68,7 @@ app.get('/encrypted/:encryptedFile', async (req, res) => {
     let decrypted = decipher.update(Buffer.from(encryptedFile, 'base64'));
     decrypted = Buffer.concat([decrypted, decipher.final()]);
 
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Disposition', `attachment; filename="${decodeURIComponent(filename)}"`);
     res.send(decrypted);
   } catch (err) {
     console.error(err);
